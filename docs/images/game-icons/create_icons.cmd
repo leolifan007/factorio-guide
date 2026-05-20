67a28e72adcb19ce6a11472da021d9ea02b7f631
leolifan007 <leolifan007@gmail.com>
2026-05-20 18:53:39 +0800

    注销 AdSense 广告代码（等审核通过后再启用），修复 .gitignore 跟踪 docs/ 目录

---
+++ docs/images/game-icons/create_icons.cmd
@@ -0,0 +1,40 @@
+@echo off
+set ICONDIR=..\icons
+set GAMEDIR=.
+
+copy %ICONDIR%\gear.svg %GAMEDIR%\iron_gear.svg /Y >nul
+copy %ICONDIR%\green-circuit.svg %GAMEDIR%\circuit_green.svg /Y >nul
+copy %ICONDIR%\iron-plate.svg %GAMEDIR%\iron_plate.svg /Y >nul
+copy %ICONDIR%\copper-plate.svg %GAMEDIR%\copper_plate.svg /Y >nul
+copy %ICONDIR%\steel-plate.svg %GAMEDIR%\steel_plate.svg /Y >nul
+copy %ICONDIR%\copper-cable.svg %GAMEDIR%\copper_cable.svg /Y >nul
+copy %ICONDIR%\red-circuit.svg %GAMEDIR%\circuit_red.svg /Y >nul
+copy %ICONDIR%\blue-circuit.svg %GAMEDIR%\circuit_blue.svg /Y >nul
+copy %ICONDIR%\sulfur.svg %GAMEDIR%\sulfur.svg /Y >nul
+copy %ICONDIR%\plastic.svg %GAMEDIR%\plastic_bar.svg /Y >nul
+copy %ICONDIR%\pipe.svg %GAMEDIR%\pipe.svg /Y >nul
+copy %ICONDIR%\coal.svg %GAMEDIR%\coal.svg /Y >nul
+copy %ICONDIR%\stone.svg %GAMEDIR%\stone.svg /Y >nul
+copy %ICONDIR%\uranium.svg %GAMEDIR%\uranium.svg /Y >nul
+copy %ICONDIR%\nuclear-reactor.svg %GAMEDIR%\nuclear_reactor.svg /Y >nul
+copy %ICONDIR%\wall.svg %GAMEDIR%\wall.svg /Y >nul
+copy %ICONDIR%\gun-turret.svg %GAMEDIR%\gun_turret.svg /Y >nul
+copy %ICONDIR%\laser-turret.svg %GAMEDIR%\laser_turret.svg /Y >nul
+copy %ICONDIR%\assembler.svg %GAMEDIR%\assembler.svg /Y >nul
+copy %ICONDIR%\assembler.svg %GAMEDIR%\engine.svg /Y >nul
+copy %ICONDIR%\furnace.svg %GAMEDIR%\furnace.svg /Y >nul
+copy %ICONDIR%\chemical-plant.svg %GAMEDIR%\chemical_plant.svg /Y >nul
+copy %ICONDIR%\oil-refinery.svg %GAMEDIR%\oil_refinery.svg /Y >nul
+copy %ICONDIR%\transport-belt.svg %GAMEDIR%\transport_belt.svg /Y >nul
+copy %ICONDIR%\splitter.svg %GAMEDIR%\splitter.svg /Y >nul
+copy %ICONDIR%\inserter.svg %GAMEDIR%\inserter.svg /Y >nul
+copy %ICONDIR%\roboport.svg %GAMEDIR%\roboport.svg /Y >nul
+copy %ICONDIR%\battery.svg %GAMEDIR%\battery.svg /Y >nul
+copy %ICONDIR%\beacon.svg %GAMEDIR%\beacon.svg /Y >nul
+copy %ICONDIR%\rocket-fuel.svg %GAMEDIR%\rocket_fuel.svg /Y >nul
+copy %ICONDIR%\rocket-silo.svg %GAMEDIR%\rocket_silo.svg /Y >nul
+copy %ICONDIR%\construction-robot.svg %GAMEDIR%\construction_robot.svg /Y >nul
+copy %ICONDIR%\productivity-module.svg %GAMEDIR%\productivity_module.svg /Y >nul
+copy %ICONDIR%\low-density-structure.svg %GAMEDIR%\low_density.svg /Y >nul
+copy %ICONDIR%\locomotive.svg %GAMEDIR%\locomotive.svg /Y >nul
+echo ALL DONE
